87610dac4bad25e22dccbc4f183adc97432154d5
Maxwell Narten <maxwellnarten@gmail.com>
2017-11-27 22:09:20 -0500

    Some component set-up

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Search from './Search';
+
 import logo from '../logo.svg';
 import '../css/style.css';
 
@@ -10,9 +12,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload. Is this thing on, now?
-        </p>
+        <Search />
       </div>
     );
   }
